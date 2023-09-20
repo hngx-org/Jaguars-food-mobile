704695daaaa88c8e4fbd626795c4d43bc58b628a
Clement <baafik05@gmail.com>
2023-09-20 18:39:50 +0000

    making changes to succeful lunch screen to branch

--- lib/core/config/router_config.dart
+++ lib/core/config/router_config.dart
@@ -5,7 +5,7 @@ import 'package:jaguar_foods_mobile/3_presentation/onboarding/screens/double_lun
 import 'package:jaguar_foods_mobile/3_presentation/success_screen.dart';
 import 'package:jaguar_foods_mobile/common/constants/route_constant.dart';
 import 'package:jaguar_foods_mobile/3_presentation/onboarding/screens/screens_barrel.dart';
-import 'package:jaguar_foods_mobile/3_presentation/onboarding/successful_lunch.dart';
+import 'package:jaguar_foods_mobile/3_presentation/onboarding/screens/successful_lunch.dart';
 
 final GoRouter routerConfig = GoRouter(
   initialLocation: RoutesPath.splashScreen,
